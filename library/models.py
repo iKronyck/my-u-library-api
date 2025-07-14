d518f445bd681d7e4921505452b55a31c974f9d5
Israel Hernández <israel.devalfaro@gmail.com>
2025-07-13 23:22:08 -0600

    feat: update Book model to use UUID as primary key

--- library/models.py
+++ library/models.py
@@ -1,11 +1,17 @@
 from django.db import models
 
 # Create your models here.
+import uuid
 from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
 
 
 class Book(models.Model):
+    id = models.UUIDField(
+        primary_key=True,
+        default=uuid.uuid4,
+        editable=False
+    )
     title = models.CharField(max_length=200, verbose_name="Title")
     author = models.CharField(max_length=100, verbose_name="Author")
     published_year = models.PositiveIntegerField(
